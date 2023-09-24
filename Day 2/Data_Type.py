12192be55887218b84fece7532a8d8ab4fa3d8c4
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-09-25 01:24:05 +0400

    /

---
+++ Day 2/Data_Type.py
@@ -0,0 +1,8 @@
+# String
+print("hello"[0])
+
+# BMI Calculator
+height = float(input("enter your height in m \n"))
+weight = float(input("enter your weight in kg \n"))
+BMI = weight / (height * height)
+print("your BMI IS :" + str(BMI))
--- Day 2/Quiz_Day 2.py
+++ Day 2/Quiz_Day 2.py
@@ -2,7 +2,7 @@ print("welcome to the tip calculator")
 
 total_bill = float(input("what was the total bills ? \n"))
 people = int(input("how many people to split the bill? \n"))
-tip = input("what percentage tip would you like to give ? 10 ,12 or 15 ?\n ")
+tip = int(input("what percentage tip would you like to give ? 10 ,12 or 15 ?\n "))
 
 
 if tip == 10:
@@ -14,6 +14,10 @@ elif tip == 15:
 else:
     tip=0
 
-bill_plus_tip = int(tip) + total_bill
+bill_plus_tip = float(tip) + total_bill
 bill_per_person = bill_plus_tip / people
-print("Each person should pay: $" + str(round(bill_per_person, 1)))
+# bill_per_person = round(bill_plus_tip / people,2)
+
+print("Each person should pay: $" + str(round(bill_per_person,2 )))
+# print(f"Each person should pay: $ { str(bill_per_person)}" )
+
