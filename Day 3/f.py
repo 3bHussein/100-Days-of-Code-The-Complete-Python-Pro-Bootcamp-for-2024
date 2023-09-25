9a9a66ec3f7375299abe9fd65769a7af7c917cb8
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-09-26 00:30:41 +0400

    Update f.py

---
+++ Day 3/f.py
@@ -0,0 +1,11 @@
+print("welcome to the rollercoaster")
+
+height= int(input("what is your height in cm ?\n"))
+
+
+if height >120:
+    print("you can please")
+    
+else:
+    print("please wait until given height")
+            
