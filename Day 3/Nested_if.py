6e83afdeb188cb93c5792bdb8f67c9f2fbe29617
3bHussein <39864308+3bHussein@users.noreply.github.com>
2023-09-26 23:54:55 +0400

    Create Nested_if.py

---
+++ Day 3/Nested_if.py
@@ -0,0 +1,23 @@
+
+print("welcome to the rollercoaster")
+
+height= int(input("what is your height in cm ?\n"))
+
+
+if height >=120:
+    print("you can ride rollercoaster ")
+    age=int(input("what is your age ? \n"))
+    if age <=18:
+        print("you will pay 8$")
+    
+    else:
+       print("your pay 14$")
+    
+    
+    
+    
+
+    
+else:
+    print("please wait until given height")
+            
