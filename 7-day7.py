f777edd5c7aebd47734869f6f1caeb51f662852e
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2024-09-09 17:19:15 +0400

    Update 7-day7.py

--- 7-day7.py
+++ 7-day7.py
@@ -6,7 +6,8 @@ word_list=["apple","camel","aardvarka"]
 
 chose_word=random.choice(word_list)
 print(chose_word)
-guess=input("guess the word \no").lower()
+
+
 
 placeholder =""
 word_lenght= len(chose_word)
@@ -14,14 +15,23 @@ for positon in range(word_lenght):
     placeholder +='-'
 
 # print(placeholder)
+gameover=False
+correct_letter=[]
 
-display=""
-for letter in chose_word:
-    if letter== guess:
-        display += letter
+while not gameover:
+    guess=input("guess the word \n").lower()
+
+    display=""
+    for letter in chose_word:
+        if letter== guess:
+            display += letter
+        else:
+            display +="-"
+
+    print(display)
+    if "_" not in display:
+        gameover=True
+        print("you Win")
     else:
-        display +="-"
-
-print(display)
 # 
 #
